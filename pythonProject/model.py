9e0db5d1f8d64501ed2fb6b888105f5d6f4ea86c
tristan-gajsek <tristan.gajsek@student.um.si>
2024-05-30 09:16:24 +0200

    PROJECT-81 Refactor all model logic into a class for convenience

--- pythonProject/model.py
+++ pythonProject/model.py
@@ -7,14 +7,6 @@ from torch.utils.data import DataLoader
 from torchvision import models
 from torchvision.datasets import ImageFolder
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Device: {device}")
-
-classes = 4
-epochs = 100
-batch_size = 32
-learning_rate = 0.001
-
 transform = transforms.Compose(
     [
         transforms.Pad(4),
@@ -24,41 +16,54 @@ transform = transforms.Compose(
     ]
 )
 
-print("Loading dataset")
-dataset_path = argv[1]
-train_dataset = ImageFolder(dataset_path, transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_dataset = ImageFolder(dataset_path, transform=transforms.ToTensor())
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-print("Creating model")
-model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
-model.fc = nn.Linear(model.fc.in_features, classes)
-model = model.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+class Model:
+    def __init__(self, classes: int = 2):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"Device: {self.device}")
 
-print("Training model")
-for epoch in range(epochs):
-    loss_avg = 0
+        self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
+        self.model.fc = nn.Linear(self.model.fc.in_features, classes)
+        self.model = self.model.to(self.device)
 
-    for i, (images, labels) in enumerate(train_loader):
-        images = images.to(device)
-        labels = labels.to(device)
+    def save(self, path: str):
+        print("Saving model")
+        torch.save(self.model.state_dict(), path)
 
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        loss_avg += loss
+    def load(self, path: str):
+        pass
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    def train(self, path: str, epochs: int, batch_size: int, learning_rate: float):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
+        dataset = ImageFolder(path, transform=transform)
+        loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-        print(f"\r{epoch + 1}/{epochs}: {i / len(train_loader) * 100:.0f}%", end="")
+        print("Training model")
+        for epoch in range(epochs):
+            loss_avg = 0
 
-    loss_avg /= len(train_loader)
-    print(f"\r{epoch + 1}/{epochs}: {loss_avg}")
-    loss_avg = 0
+            for i, (images, labels) in enumerate(loader):
+                images = images.to(self.device)
+                labels = labels.to(self.device)
 
-print("Saving model")
-torch.save(model.state_dict(), "resnet.pt")
+                outputs = self.model(images)
+                loss = criterion(outputs, labels)
+                loss_avg += loss
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                print(f"\r{epoch + 1}/{epochs}: {i / len(loader) * 100:.0f}%", end="")
+
+            loss_avg /= len(loader)
+            print(f"\r{epoch + 1}/{epochs}: {loss_avg}")
+            loss_avg = 0
+
+    def eval(self):
+        pass
+
+
+model = Model(4)
+model.train(argv[1], 3, 32, 0.001)
