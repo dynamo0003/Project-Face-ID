438ba9c80bdc52933be80d61a4cd5d8d11b469cc
TimSesel <tim.sesel@student.um.si>
2024-06-03 13:47:13 +0200

    PROJECT 27 comments
    Commented what i've done

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# This should be changed to fit server's address
+# Current path "http_://IP:port/face-recognition/authenticate"
 @app.route('/face-recognition/authenticate', methods=['POST'])
 def authenticate():
     ...
