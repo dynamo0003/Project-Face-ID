142cbbae2b326f518e73c586f2256cfebb0c97e4
TimSesel <tim.sesel@student.um.si>
2024-06-03 13:44:08 +0200

    PROJECT-27 definitions
    Defined temporary route of server and method to POST.
    
    Defined function for authentication

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 from model import Model
 from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route('/face-recognition/authenticate', methods=['POST'])
+def authenticate():
+    ...
+
 if __name__ == '__main__':
     pass
