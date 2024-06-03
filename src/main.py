8c5668937a6922337a0815135c524018b4f7f9c5
TimSesel <tim.sesel@student.um.si>
2024-06-03 14:22:39 +0200

    PROJECT-27 image handling
    Image is received, saved then the absolute path to the saved image is saved.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 from model import Model
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
 # This should be changed to fit server's address
-# Current path "http_://IP:port/face-recognition/authenticate"
+# Current path "http_://127.0.0.1:5000/face-recognition/authenticate"
 @app.route('/face-recognition/authenticate', methods=['POST'])
 
 def authenticate():
+    # Replace "temp_path" with actual path once connected to server
     model_path = "temp_path"
-    model = Model()
+    temp_image_name = "temp_img.png"
 
+    model = Model()
     model.load(model_path)
+
+    img = request.files['image']
+    img.save(temp_image_name)
+    abs_img_path = os.path.abspath(temp_image_name)
 
 if __name__ == '__main__':
     pass
