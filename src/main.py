04ecbfe9901708a1fbe5ef8a141aa0ff5f3c5a5f
TimSesel <tim.sesel@student.um.si>
2024-06-03 14:26:56 +0200

    PROJECT-27 eval and removal
    Image should now be evaluated then removed from the server
    
    TODO: need to change model.eval return value + based on recieved data see if face ID was a success

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 # This should be changed to fit server's address
 # Current path "http_://127.0.0.1:5000/face-recognition/authenticate"
 @app.route('/face-recognition/authenticate', methods=['POST'])
-
 def authenticate():
     # Replace "temp_path" with actual path once connected to server
     model_path = "temp_path"
@@ -19,6 +18,10 @@ def authenticate():
     img = request.files['image']
     img.save(temp_image_name)
     abs_img_path = os.path.abspath(temp_image_name)
+
+    model.eval(abs_img_path)
+
+    os.remove(abs_img_path)
 
 if __name__ == '__main__':
     pass
