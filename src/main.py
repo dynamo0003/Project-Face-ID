9710788e9d3c80dfc96c2d2771d525f86ca17e62
TimSesel <tim.sesel@student.um.si>
2024-06-03 13:52:31 +0200

    PROJECT-27 Load model
    Added code to load saved model.

--- src/main.py
+++ src/main.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 # This should be changed to fit server's address
 # Current path "http_://IP:port/face-recognition/authenticate"
 @app.route('/face-recognition/authenticate', methods=['POST'])
+
 def authenticate():
-    ...
+    model_path = "temp_path"
+    model = Model()
+
+    model.load(model_path)
 
 if __name__ == '__main__':
     pass
