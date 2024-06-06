89b129d19a7f968ee54719f5f78048c23717e0ec
dynamo <zigagorenc1@gmail.com>
2024-06-06 17:13:50 +0200

    PROJECT-83 Added custom gaussian blur

--- src/augment.py
+++ src/augment.py
@@ -20,15 +20,11 @@ class CustomAffine:
         return F.affine(img, self.angle, self.translate, self.scale, self.shear)
 
 class CustomPerspective:
-    def __init__(self, distortion_scale=0.5, p=0.5, zoom_factor=1):
+    def __init__(self, distortion_scale, zoom_factor):
         self.distortion_scale = distortion_scale
-        self.p = p
         self.zoom_factor = zoom_factor
 
     def __call__(self, img):
-        if random.uniform(0, 1) > self.p:
-            return img
-
         startpoints, endpoints = self.get_params(IMAGE_SIZE, self.distortion_scale, self.zoom_factor)
         return F.perspective(img, startpoints, endpoints)
 
@@ -59,6 +55,14 @@ class CustomPerspective:
         endpoints = [topleft, topright, botright, botleft]
         return startpoints, endpoints
 
+class CustomGaussianBlur:
+    def __init__(self, kernel_size, sigma=(0.1, 2.0)):
+        self.kernel_size = kernel_size
+        self.sigma = sigma
+
+    def __call__(self, img):
+        return F.gaussian_blur(img, self.kernel_size, self.sigma)
+
 IMAGE_SIZE = (224, 224)
 
 # TODO potentially add pickle array functionality
@@ -79,7 +83,7 @@ def get_transforms(skip_extra=False):
         # Brigthness, contrast, saturation (1-3 times)
         for _ in range(random.randint(0, 2)):
             if random.randint(0, 4) == 0:  # 20% chance
-                trans.append(t.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3))
+                trans.append(t.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2))
         # Crop area where size is at least 3/4 or crop around the center
         if random.randint(0, 100):
             trans.append(t.RandomResizedCrop(IMAGE_SIZE, scale=(0.75, 1)))
@@ -87,12 +91,15 @@ def get_transforms(skip_extra=False):
             trans.append(t.CenterCrop(IMAGE_SIZE))
         # Rotate randomly by 0 - 20°
         trans.append(t.RandomRotation(random.randint(0, 20), fill=128))
-        # Custom affine and shear (12.5%)
-        if random.randint(0, 7) == 0:
+        # Custom affine and shear (10%)
+        if random.randint(0, 9) == 0:
             trans.append(CustomAffine(translate=(10, 10), scale=1, shear=random.randint(-10, 10)))
-        # Custom perspective (12.5%) -- also zooms slightly to mitigate black borders
-        if random.randint(0, 7) == 0:
-            trans.append(CustomPerspective(distortion_scale=0.2, p=1, zoom_factor=0.9))
+        # Custom perspective (10%) -- also zooms slightly to mitigate black borders
+        if random.randint(0, 9) == 0:
+            trans.append(CustomPerspective(distortion_scale=0.2, zoom_factor=0.8))
+        # Custom gaussian blur (1%)
+        if random.randint(0, 99) == 0:
+            trans.append(CustomGaussianBlur(kernel_size=5))
 
     trans.append(t.Resize(IMAGE_SIZE))
     return transforms.Compose(trans)
