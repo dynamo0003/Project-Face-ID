69c8ba43aaf4c2766b78e4cead771e44bda45a93
TimSesel <tim.sesel@student.um.si>
2024-06-09 11:07:14 +0200

    Reformation
    Reformed how data is handled and when training or authentication take place

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-import platform
 from model import Model
 from flask import Flask, request, jsonify
 import os
+import cv2
 
 app = Flask(__name__)
 
@@ -14,35 +14,75 @@ def authenticate():
     user = request.form['user']
 
     model_path = f"Project-Face-ID/models/{user}.pt"
-    if not os.path.exists(model_path):
-        # return jsonify({"error": f"Model file {model_path} does not exist"}), 400
-        # Code to train new model
-        pass
+    model = Model(classes=4)
+
+    if 'purpose' not in request.form:
+        return jsonify({"error": "No purpose part in the request"}), 400
+    purpose = request.form['purpose']
+
+    if purpose == "train":
+        if os.path.exists(model_path):
+            return jsonify({"error": "Model for this user already exists, cannot train a new one"}), 400
+        else:
+            if 'video' not in request.files:
+                return jsonify({"error": "No video part in the request"}), 400
+            vid = request.files['video']
+
+            if vid.filename == '':
+                return jsonify({"error": "No image path found"}), 400
+
+            temp_video_name = "temp_img.png"
+            vid.save(temp_video_name)
+            abs_vid_path = os.path.abspath(temp_video_name)
+
+            cap = cv2.VideoCapture(abs_vid_path)
+            frame_count = 0
+
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                
+                training_images_path = "Project-Face-ID/trainingImages/"
+                frame_filename = os.path.join(training_images_path, f"frame_{frame_count:04d}.png")
+                cv2.imwrite(frame_filename, frame)
+                frame_count += 1
+
+            cap.release()
+            os.remove(abs_vid_path)
+
+            model.train()
+            model.save(model_path)
+
+            return jsonify({"result": 1})
+    elif purpose == "auth":
+        if not os.path.exists(model_path):
+            return jsonify({"error": "There is no model trained for this user"}), 400
+        else:
+            model.load(model_path)
+
+            if 'image' not in request.files:
+                return jsonify({"error": "No image part in the request"}), 400
+            img = request.files['image']
+
+            if img.filename == '':
+                return jsonify({"error": "No image path found"}), 400
+
+            temp_image_name = "temp_img.png"
+            img.save(temp_image_name)
+            abs_img_path = os.path.abspath(temp_image_name)
+
+            choice, threshold, probs = model.eval(abs_img_path)
+            os.remove(abs_img_path)
+
+            for i, p in enumerate(probs):
+                if i == choice:
+                    if p >= threshold:
+                        return jsonify({"result": 1}) # Image class was found
+
+            return jsonify({"result": 0}) # Image class was not found
     else:
-        classes = 4
-        model = Model(classes)
-        model.load(model_path)
-
-        if 'image' not in request.files:
-            return jsonify({"error": "No image part in the request"}), 400
-        img = request.files['image']
-
-        if img.filename == '':
-            return jsonify({"error": "No image path found"}), 400
-
-        temp_image_name = "temp_img.png"
-        img.save(temp_image_name)
-        abs_img_path = os.path.abspath(temp_image_name)
-
-        choice, threshold, probs = model.eval(abs_img_path)
-        os.remove(abs_img_path)
-
-        for i, p in enumerate(probs):
-            if i == choice:
-                if p >= threshold:
-                    return jsonify({"result": 1}) # Image class was found
-        
-        return jsonify({"result": 0}) # Image class was not found
+        return jsonify({"error": "The purpose is invalid"}), 400
 
 if __name__ == '__main__':
     # Add debug=True for debugging
