e29d295461823e0eef67935aed24af5c9c7929cd
TimSesel <tim.sesel@student.um.si>
2024-06-09 22:36:06 +0200

    troubleshooting
    troubleshooting

--- src/main.py
+++ src/main.py
@@ -15,8 +15,8 @@ def authenticate():
         return jsonify({"error": "No user part in the request"}), 400
     user = request.form['user']
 
-    model_path = os.path.join("/models", f"{user}.pt")
-    #model_path = f"/models/{user}.pt"
+    #model_path = os.path.join("/models", f"{user}.pt")
+    model_path = f"/models/{user}.pt"
     model = Model(classes=4)
 
     if 'purpose' not in request.form:
